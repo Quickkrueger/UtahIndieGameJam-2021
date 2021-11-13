6fe6bca1499bc3638e9267e1bc2b610b79f74eb4
Marshall Krueger <mgkrueger4@gmail.com>
2021-11-13 14:12:17 -0700

    Setting up character swapping

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
 
@@ -10,6 +11,9 @@ public class CharacterController : MonoBehaviour
     public CollectionSO collectedCharacters;
     public CharacterData currentCharacterData;
     public ControlsData characterControls;
+
+    public UnityEvent StartSwap;
+    public UnityEvent EndSwap;
 
     private Rigidbody2D characterRB;
     private Collider2D characterCollider;
@@ -43,6 +47,11 @@ public class CharacterController : MonoBehaviour
                     break;
                 case "Fly": characterInputs.actions[i].performed += Fly;
                     characterInputs.actions[i].canceled += Fly;
+                    characterInputs.actions[i].Enable();
+                    break;
+                case "Swap Character":
+                    characterInputs.actions[i].performed += SwapCharacter;
+                    characterInputs.actions[i].canceled += SwapCharacter;
                     characterInputs.actions[i].Enable();
                     break;
             }
@@ -112,6 +121,19 @@ public class CharacterController : MonoBehaviour
         if (characterControls.canFly && value.performed)
         {
             characterRB.AddForce(Vector2.up * currentCharacterData.characterStats.jumpHeight);
+        }
+    }
+
+    private void SwapCharacter(CallbackContext value)
+    {
+
+        if(value.performed && collectedCharacters.collectables.Count > 1)
+        {
+            StartSwap.Invoke();
+        }
+        else
+        {
+            EndSwap.Invoke();
         }
     }
 
